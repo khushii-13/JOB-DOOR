5911d626d095dcfd2e9f96ed7a805a55ccb874cd
khushii-13 <khushichaudhary904@gmail.com>
2024-10-15 11:10:20 +0530

    setup basic code

---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import {View , Text} from "react-native";
+
+const Home =()=>{
+    return (
+        <View>
+            <Text>
+                welcome to Home !!
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
